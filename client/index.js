dbdb19ae09a56b7d729de600c25bd1aba3801a1a
Aristides Staffieri <aristides@aristidesstaffieri.com>
2018-06-04 16:36:27 -0600

    add components for messaging, state for active messenger, channels for messaging

--- client/index.js
+++ client/index.js
@@ -5,27 +5,99 @@ import io from 'socket.io-client'
 
 import './styles/index.css'
 
-class SidePanel extends React.Component {
+class Messages extends React.Component {
   constructor(props) {
     super(props)
   }
 
   render() {
     return (
+      <div>
+        { this.props.messages }
+      </div>
+    )
+  }
+}
+
+class InputField extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value)
+    event.preventDefault()
+    this.props.onSubmit(this.state.value)
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this.handleSubmit } className="chat_form">
+        <input name="chat_input" type="text" value={ this.state.value } onChange={ this.handleChange } />
+        <input type="submit" value="Send" />
+      </form>
+    )
+  }
+}
+
+InputField.propTypes = {
+  onSubmit: P.func
+}
+
+InputField.defaultProps = {
+  onSubmit: () => false //no op
+}
+
+class SidePanel extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.onClickItem = this.onClickItem.bind(this)
+  }
+
+  onClickItem(item) {
+    this.props.onClickItem(item)
+  }
+
+  render() {
+    const { activeItem } = this.props
+
+    return (
       <div className="active_users_panel">
         <h1 className="panel_title">Active Users</h1>
-        { this.props.items.map(item => <h5 className="panel_items" key={ item }>{ item }</h5>) }
+        { this.props.items.map(item => {
+          return (
+            <h5
+            className={ `panel_items ${ activeItem === item && 'active' }` }
+            onClick={ this.onClickItem.bind(null, item) }
+            key={ item }>
+              { item }
+            </h5>
+          )
+        }) }
       </div>
     )
   }
 }
 
 SidePanel.propTypes = {
-  items: P.array
+  items: P.array,
+  // activeItem: P.oneOfType([P.string, P.null]),
+  onClickItem: P.func
 }
 
 SidePanel.defaultProps = {
-  items: []
+  items: [],
+  activeItem: null,
+  onClickItem: () => false //no op
 }
 
 class ChatApp extends React.Component {
@@ -33,24 +105,57 @@ class ChatApp extends React.Component {
     super(props)
     this.state = {
       socket: io('http://localhost:3000'),
-      connections: []
+      connections: [],
+      userId: null,
+      activeItem: null,
+      messages: []
     }
+
+    this.setActiveItem = this.setActiveItem.bind(this)
+    this.sendMessage = this.sendMessage.bind(this)
 
     this.state.socket.on('active connections', connections => this.setState({
       connections: connections
     }))
+
+    this.state.socket.on('userId', id => this.setState({
+      userId: id
+    }))
+
+    this.state.socket.on('message', msg => this.setState({
+      messages: this.state.messages.concat([msg])
+    }))
   }
 
+  setActiveItem(item) {
+    this.setState({
+      activeItem: item
+    })
+  }
+
+  sendMessage(msg) {
+    console.log(this.state.socket)
+    this.state.socket.emit('private_msg', { id: this.state.activeItem, msg })
+  }
+
+  // sendMessage(msg) {
+  //   // this.state.socket.emit
+  // }
+
   render () {
-    const { connections } = this.state
-    console.log(connections)
+    const { connections, userId, activeItem, messages } = this.state
     return [
       <SidePanel
         key="active_users_panel"
-        items={ connections }
+        items={ connections.filter(id => id !== userId) }
+        activeItem={ activeItem }
+        onClickItem={ this.setActiveItem }
       />,
       <div className="active_chat_panel" key="active_chat_panel">
-
+        <div className="conversation">
+          <Messages messages={ messages } />
+        </div>
+        <InputField onSubmit={ this.sendMessage } />
       </div>
     ]
   }
--- server/index.js
+++ server/index.js
@@ -16,12 +16,19 @@ let members = []
 io.on('connection', function(socket){
   console.info('a user connected', socket.id)
   members.push(socket.id)
+  // emit active members and your userId
   io.emit('active connections', members)
+  socket.emit('userId', socket.id)
 
   socket.on('disconnect', function(){
     console.info('user disconnected', this.id)
     members = members.filter(member => member !== this.id)
     io.emit('active connections', members)
+  })
+
+  socket.on('private_msg', function(data){
+    console.info('message received', data)
+    socket.to(data.id).emit('message', data.msg)
   })
 
 
